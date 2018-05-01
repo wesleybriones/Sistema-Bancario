cc5e0089bbcc64586d7c4c39b6fdd3c926f076b2
dacore1 <alidaryousef@gmail.com>
2018-05-01 13:27:10 -0600

    Creación de build.xml ant

--- Sistema-Bancario/src/funcionalidades/principal.java
+++ Sistema-Bancario/src/funcionalidades/principal.java
@@ -5,6 +5,7 @@ public class principal {
     public static Registro reg;
     public static void main(String [] args)
     {
+        System.out.println("----------- MAIN --------------");
         reg = new Registro();
         reg.setVisible(true);
     }
