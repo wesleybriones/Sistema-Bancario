44c12a29a8021f3f5e78d2fb557fa504e8f3747f
Jean9611 <jruizg96.11@gmail.com>
2018-05-01 06:24:27 -0600

    registro fix

--- Sistema-Bancario/src/funcionalidades/principal.java
+++ Sistema-Bancario/src/funcionalidades/principal.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package funcionalidades;
 
@@ -8,22 +7,5 @@ public class principal {
     {
         reg = new Registro();
         reg.setVisible(true);
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package funcionalidades;
-
-/**
- *
- * @author dacore
- */
-public class principal {
-    public static void main(String [] args)
-    {
-        System.out.println("Hola mundo");
->>>>>>> 64465d9a8e9db52a3eec7b63926f6db59c298ff3
     }
 }
